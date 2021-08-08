d00a31964d99cb1484cbd2ed525600cf00bf30e1
Xilef12000 <manuel.koenig@online.de>
2021-08-08 14:38:34 +0200

    changes.

--- test_sketch/test_sketch.ino
+++ test_sketch/test_sketch.ino
@@ -4,22 +4,26 @@
 //by Manuel König             //
 ////////////////////////////////
 
-#define diods 2 //number of used photodiods
-#define diod_pin 0 //(analog)pin of the first photodio, normally (analog)pin 0
+#define diodes 3 //number of used photo-diodes
+#define diode_pin 0 //(analog)pin of the first photodio, normally (analog)pin 0
 #define baudrate 9600 // Serial comunication speed/baudrate, normally 9600
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(baudrate);
   //Serial.println("\n////////////////////////////////\n//Laser-Parkour test sketch   //\n//                            //\n//by Manuel König             //\n////////////////////////////////\n");
-  for (int i = diod_pin; i <= diod_pin + diods - 1; i++) {
-    //Serial.println("using photodiod on pin " + String(i));
+  for (int i = diode_pin; i <= diode_pin + diodes - 1; i++) {
+    //Serial.println("using photo-diode on pin " + String(i));
   }
+  for (int i = diode_pin; i <= diode_pin + diodes - 1; i++) {
+    Serial.print("photo-diode" + String(i));
+  }
+  Serial.println("uT");
   //Serial.println("");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for (int i = diod_pin; i <= diod_pin + diods - 1; i++) {
+  for (int i = diode_pin; i <= diode_pin + diodes - 1; i++) {
     Serial.print(String(analogRead(i)));
     Serial.print("  ");
   }
