bf60f2a9aa7fc434b7d40de3608a98925272dfb2
Xilef12000 <manuel.koenig@online.de>
2021-08-11 16:48:49 +0200

    class stuff

--- sketch/laser_parkour.h
+++ sketch/laser_parkour.h
@@ -1,4 +1,5 @@
 class laser_parkour {
+<<<<<<< Updated upstream
   public:
     int diodes_pin = 0;
     int start_pin = 12;
@@ -8,5 +9,17 @@ class laser_parkour {
     }
     unsigned int game() {
 
+=======
+  private:
+    int diodes_pin = 1;
+    int start_pin = 12;
+    int stop_pin = 11;
+  public:
+    void begin(int diodes_pin_local, int  start_pin_local, int  stop_pin_local) {
+
+    }
+    void game() {
+      Serial.println(diodes_pin);
+>>>>>>> Stashed changes
     }
 };
