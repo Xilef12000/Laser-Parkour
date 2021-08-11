f5ba47388b7bab9ca62bfab0fdf972a5fd4135d3
Xilef12000 <manuel.koenig@online.de>
2021-08-11 19:05:27 +0200

    delete errors

--- sketch/laser_parkour.h
+++ sketch/laser_parkour.h
@@ -1,25 +1,22 @@
 class laser_parkour {
-<<<<<<< Updated upstream
-  public:
-    int diodes_pin = 0;
-    int start_pin = 12;
-    int stop_pin = 11;
-    void begin(diodes_pin, start_pin, stop_pin) {
-
-    }
-    unsigned int game() {
-
-=======
   private:
-    int diodes_pin = 1;
-    int start_pin = 12;
-    int stop_pin = 11;
+    int diodes_pin = 5;
+    int start_pin = 9;
+    int stop_pin = 8;
+    void (*onstart_function);
   public:
-    void begin(int diodes_pin_local, int  start_pin_local, int  stop_pin_local) {
-
+    void begin(int diodes_pin_local, int start_pin_local, int stop_pin_local) {
+      diodes_pin = diodes_pin_local;
+      start_pin = start_pin_local;
+      stop_pin = stop_pin_local;
     }
     void game() {
-      Serial.println(diodes_pin);
->>>>>>> Stashed changes
+
+    }
+    void start() {
+      (*onstart_function)()
+    }
+    void onstart(void (*function)()) {
+      (*onstart_function) = (*function);
     }
 };
--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -5,52 +5,32 @@
 //and Manuel König            //
 ////////////////////////////////
 
-<<<<<<< Updated upstream
-#include "laserMonitoring.h"
-#include "nameInput.h"
-#include "results.h"
-#include "sort.h"
-#include "startInfo.h"
-
-#define penalty 10000 //penalty time for Laser interruption in ms
-=======
 #include "laser_parkour.h"
 
->>>>>>> Stashed changes
 #define diodes_pin 0 //(analog)pin of the photo-diodes, normally (analog)pin 0
 #define start_pin 12 //pin of start button, normally pin 13
 #define stop_pin 11 //pin of stop button, normally pin 12
 #define baudrate 9600 //Serial comunication speed/baudrate, normally 9600
-<<<<<<< Updated upstream
-#define trigger_value 180 //value to trigger
-#define speaker_pin 9 //pin of the speaker
-#define start_beep 400 //tone when start button gets pressed
-#define stop_beep 400 //tone when stop button gets pressed
-#define laser_beep 500 //tone when laser gets interrupted
-#define beep_length 300 //tone length
-
-laser_parkour parkour1;
-=======
->>>>>>> Stashed changes
 
 laser_parkour parkour;
+void started(){
+  Serial.println("start");
+}
+void stopped(){
+  Serial.println("stop");
+}
+void failed(){
+  Serial.println("fail");
+}
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(baudrate); //Start serial comunication
   Serial.setTimeout(10); //timeout for reciving strings in ms
-  parkour.begin();
+  parkour.begin(diodes_pin, start_pin, stop_pin);
+  parkour.onstart(started);
+  parkour.onstop(stopped);
+  parkour.onfail(failed);
   parkour.game();
 }
 
 void loop() {
-<<<<<<< Updated upstream
-  // put your main code here, to run repeatedly:
-  penalty_time = 0; //reseting the total penalty time
-  userName = nameInput(); //asking for a user Input
-  laserMonitoring(); //monitor the lasers and buttons
-  results(start_time, stop_time, penalty_time); //calculate results
-=======
-
-
->>>>>>> Stashed changes
 }
