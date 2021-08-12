9dc554266ee9c96ebad3f015c70b7b07043d028c
Xilef12000 <manuel.koenig@online.de>
2021-08-12 17:54:50 +0200

    changes

--- sketch/laser_parkour.h
+++ sketch/laser_parkour.h
@@ -3,20 +3,41 @@ class laser_parkour {
     int diodes_pin = 5;
     int start_pin = 9;
     int stop_pin = 8;
-    void (*onstart_function);
+    int trigger_value = 255;
+    int penalty_time = 5000;
+    void (*onstart_function)();
+    void (*onstop_function)();
+    void (*onfail_function)();
   public:
-    void begin(int diodes_pin_local, int start_pin_local, int stop_pin_local) {
+    void game() {
+      while (digitalRead(start_pin) == 1) {}
+      int start_time = millis();
+      (*onstart_function)();
+      while (digitalRead(stop_pin) == 1) {
+        if (analogRead(diodes_pin) >= trigger_value) {
+          (*onfail_function)();
+          while (analogRead(diodes_pin) >= trigger_value) {}
+        }
+      }
+      (*onstop_function)();
+      //return (millis() - start_time);
+    }
+    void configurePins(int diodes_pin_local, int start_pin_local, int stop_pin_local) {
       diodes_pin = diodes_pin_local;
       start_pin = start_pin_local;
       stop_pin = stop_pin_local;
     }
-    void game() {
-
+    void configureGame(int trigger_value_local, int penalty_time_local) {
+      trigger_value = trigger_value_local;
+      penalty_time = penalty_time_local;
     }
-    void start() {
-      (*onstart_function)()
+    void onstart(void (*function_local)()) {
+      onstart_function = function_local;
     }
-    void onstart(void (*function)()) {
-      (*onstart_function) = (*function);
+    void onstop(void (*function_local)()) {
+      onstop_function = function_local;
+    }
+    void onfail(void (*function_local)()) {
+      onfail_function = function_local;
     }
 };
--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -10,27 +10,43 @@
 #define diodes_pin 0 //(analog)pin of the photo-diodes, normally (analog)pin 0
 #define start_pin 12 //pin of start button, normally pin 13
 #define stop_pin 11 //pin of stop button, normally pin 12
+#define penalty 10000 //penalty time for Laser interruption in ms
+#define trigger_value 180 //value to trigger
+
 #define baudrate 9600 //Serial comunication speed/baudrate, normally 9600
 
 laser_parkour parkour;
-void started(){
+void started() {
   Serial.println("start");
 }
-void stopped(){
+void stopped() {
   Serial.println("stop");
 }
-void failed(){
+void failed() {
   Serial.println("fail");
 }
 void setup() {
   Serial.begin(baudrate); //Start serial comunication
   Serial.setTimeout(10); //timeout for reciving strings in ms
-  parkour.begin(diodes_pin, start_pin, stop_pin);
+  //configure pins      |diodes     |start     |stop:
+  parkour.configurePins(diodes_pin, start_pin, stop_pin);
+  //configure game      |trigger value |penalty time:
+  parkour.configureGame(trigger_value, penalty);
+  //this function gets called on the start of a round:
   parkour.onstart(started);
+  //this function gets called on the end of a round:
   parkour.onstop(stopped);
+  //this function gets called on the interruptionof a laser:
   parkour.onfail(failed);
-  parkour.game();
+
+
+
 }
 
 void loop() {
+  delay(2000);
+  Serial.println("\nNew Game:");
+  parkour.game();
+  //delay(10000);
+  //exit(0);
 }
