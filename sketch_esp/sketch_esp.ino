c89de6b049ad83f558872f53a986a370371cc534
Xilef12000 <manuel.koenig@online.de>
2021-10-26 18:11:44 +0200

    working on web-interface

--- sketch_esp/sketch_esp.ino
+++ sketch_esp/sketch_esp.ino
@@ -6,17 +6,17 @@ const char* ssid = "LaserParkour";  // Enter your SSID here
 const char* password = "testpw";  //Enter your Password here
 
 // IP Address details
-IPAddress local_ip(192, 168, 1, 1);
+IPAddress local_ip(192, 168, 1, 1); //192.168.1.1
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
 
 WebServer server(80);  // Object of WebServer(HTTP port, 80 is defult)
 
-String TEXT = "no \n data";
+String TEXT = "no Top-list";
 void setup() {
   Serial.begin(9600);
 
-  TEXT.replace("\n","\\n");
+  TEXT.replace("\n","");
   //Serial.println(TEXT);
 
   // Create SoftAP
@@ -42,14 +42,14 @@ void loop() {
     /*while (Serial.available() > 0){
       TEXT += Serial.readString();
     }*/
-    TEXT.replace("\n","\\n");
+    TEXT.replace("\n","");
     //Serial.println("New Text: " + TEXT + ";");
   }
 }
 
 // HTML & CSS contents which display on web server
 String HTML = "<!DOCTYPE html>  <html>   <head>    <meta charset='utf-8'>    <title>Name Input</title>   </head>   <body>    <form action='/'>     New Player:     <br>     <input type='test' name='name'>     <input type='submit' name='submit'>    </form>    <iframe src='/table'></iframe>   </body>  </html>";
-String TABLE_1 = "<!DOCTYPE html>  <html>   <head>    <meta charset='utf-8'>    <title>Top-table</title>    <META HTTP-EQUIV='refresh' CONTENT='5'>   </head>   <body>    <p id='Top'>test</p>    <script type='text/javascript'>     var list = '";
+String TABLE_1 = "<!DOCTYPE html>  <html>   <head>    <meta charset='utf-8'>    <title>Top-table</title>    <META HTTP-EQUIV='refresh' CONTENT='5'>   </head>   <body>    <p id='Top'>ERROR: NO USEFULL DATA</p>    <script type='text/javascript'>     var list = '";
 String TABLE_2 = "';     document.getElementById('Top').innerHTML = list;    </script>   </body>  </html>";
 // Handle root url (/)
 void handle_root() {
