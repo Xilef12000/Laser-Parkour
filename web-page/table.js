22e2c6e7b70807e6ed5a3b52cf185557220c790b
Xilef12000 <manuel.koenig@online.de>
2021-11-01 14:52:47 +0100

    web-page changes

--- web-page/table.js
+++ web-page/table.js
@@ -1,3 +1,18 @@
-var list = "";
-list = list.replace(/;/g, "<br>");
-document.getElementById("Top").innerHTML = list;
+setInterval(load_json, 3000);
+var url = "table.json";
+var nocach_count = 0;
+function load_json() {
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			parsed_json = JSON.parse(this.responseText);
+			console.log(parsed_json);
+			//var list = parsed_json.place1.name;
+			//document.getElementById("Top").innerHTML = list;
+		}
+	}
+}
+xmlhttp.open("GET", url + "?nocache=" + nocach_count, true);
+xmlhttp.send();
+console.log("loaded json :" + "?nocache=" + nocach_count);
+nocach_count ++;
