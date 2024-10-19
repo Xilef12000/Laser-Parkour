32bad7c664a38379ecb0712f29d49f1c06876010
Xilef12000 <79515423+Xilef12000@users.noreply.github.com>
2024-10-19 15:30:06 +0200

    too much changes

--- Sketches/Controller/main.py
+++ Sketches/Controller/main.py
@@ -5,6 +5,8 @@ from sys import exit
 import network
 import rp2
 import socket
+import os
+import uasyncio as asyncio 
 
 # WAP config
 wap_country = 'DE'
@@ -19,49 +21,77 @@ lcd_i2c = 1
 lcd_sda = Pin(14)
 lcd_scl = Pin(15)
 
+class Sensor:
+    address = 0
+    deviceType = 0
+    deviceMode = 0
+    conStatus = 1
+    value = -1
+    def __init__ (self, address):
+        self.address = address
+        self.setMode(0)
+        sleep_ms(200)
+        self.deviceType = int.from_bytes(i2c.readfrom(self.address, 1, True), 'big')
+    def __str__(self):
+        return dict(address = hex(self.address), deviceType = self.deviceType, deviceMode = self.deviceMode, conStatus = self.conStatus, value = self.value)
+    def setMode(self, mode):
+        i2c.writeto(self.address, bytearray([0b00010000 + mode]), True)
+        self.deviceMode = mode
+    def setType(self, type):
+        i2c.writeto(self.address, bytearray([0b00100000 + type]), True)
+        self.deviceType = type
+    def readValue(self):
+        try:
+            data = i2c.readfrom(self.address, 2, True)
+            self.conStatus = 1
+            self.value = int.from_bytes(data, 'big')
+            #print(f'{hex(self.address)} : {self.value}')
+        except OSError as error:
+            self.conStatus -= 1
+            #print(f'sensor {self.address} faulty')\
+
+        
 def init():
     # init lcd
-    global lcd
-    lcd = I2cLcd(I2C(lcd_i2c, sda=lcd_sda, scl=lcd_scl, freq=100000), 0x27, 2, 16)
+    #global lcd
+    #lcd = I2cLcd(I2C(lcd_i2c, sda=lcd_sda, scl=lcd_scl, freq=100000), 0x27, 2, 16)
 
-    lcd.putstr("...")
+    #lcd.putstr("...")
 
     # init bus
     global i2c
-    i2c = I2C(bus_i2c,sda=bus_sda,scl=bus_scl,freq=100000)
+    i2c = I2C(bus_i2c,sda=bus_sda,scl=bus_scl,freq=4800) # set speed form 100000 to xxx
 
     # bus scan
     print('Scaning Sensor Bus...')
-    devices = i2c.scan()
-    if len(devices) == 0:
+    foundAdresses = i2c.scan()
+    if len(foundAdresses) == 0:
         print('No Sensor found!\nexiting...')
         exit(0)
     else:
-        print(f'{len(devices)} Sensors found')
+        print(f'{len(foundAdresses)} Sensors found')
         global sensors
         sensors = []
-        for device in devices:
-            sensors.append(dict(adrress = device, type = 'sensor', status = 1, value = -1))
-        print(sensors) 
+        for address in foundAdresses:
+            sensor = Sensor(address)
+            print(str(sensor))
+            sensors.append(sensor)
+        
+    for sensor in sensors:
+        sensor.setMode(1)
+        sensor.setType(1)
             
-def main():
+async def main():
+    asyncio.create_task(asyncio.start_server(serveClient, "192.168.4.1", 80))
+    init()
     time_last = ticks_ms() 
     while True:
-        serveWeb()
         if ticks_ms() >= time_last + 200:
-            readSensors()
+            time_last = ticks_ms() 
+            for sensor in sensors:
+                sensor.readValue()
+        await asyncio.sleep(0.1)   # Sleep for 0.1 seconds 
         
-def readSensors():
-    for sensor in sensors:
-        try:
-            data = i2c.readfrom(sensor['adrress'], 2, True)
-        except OSError as error:
-            sensor['status'] -= 1
-            #print(f'sensor {hex(sensor["adrress"])} faulty')
-            continue
-        sensor['status']
-        sensor['value'] = int.from_bytes(data, 'big')
-        #print(f'{hex(sensor["adrress"])} : {sensor['value']}')
 def createWap():
     rp2.country(wap_country)
     wap = network.WLAN(network.AP_IF)
@@ -74,32 +104,43 @@ def createWap():
     print('Standard-Gateway:', netConfig[2])
     print('DNS-Server:', netConfig[3])
 
-def createWeb():
-    addr = socket.getaddrinfo(pico_ip, 80)[0][-1]
-    global server
-    server = socket.socket()
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind(addr)
-    server.listen(1)
-    print('Server listening on', addr)
+def serveClient(reader, writer):
+    #print("Client connected")  # Print message when client is connected
+    request_line = await reader.readline()  # Read the HTTP request line
+    #print("Request:", request_line)         # Print the received request
+    # Skip HTTP request headers
+    while await reader.readline() != b"\r\n":
+        pass
 
-def serveWeb():
-    try:
-        conn, addr = server.accept()
-        #print('HTTP-Request von Client', addr)
-        request = conn.recv(1024)
-        #print('Request:', request)
-        response = '<!doctype html><html>' + str(sensors) + '<script>setTimeout("window.location.reload();", 1000)</script></html>'
-        conn.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
-        conn.send(response)
-        conn.close()
-        #print('HTTP-Response gesendet')
-        #print()
-    except OSError as e:
-        return
+    request = str(request_line)        # Convert request to string
+    request = request.split()
+    response = ''
+    if request[1] == '/':
+        file = open('index.html', 'r')
+        response = file.read()
+        file.close()
+        writer.write('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
+        writer.write(response)
+    elif request[1] == '/API':
+        response = '['
+        for sensor in sensors:
+            response += str(sensor).replace("'", '"') + ','
+        response = response[:-1]
+        response += ']'
+        writer.write('HTTP/1.0 200 OK\r\nContent-type: text/json\r\nAccess-Control-Allow-Origin: *\r\n\r\n')
+        writer.write(response)
+    else:
+        response = '404'
+        writer.write('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
+        writer.write(response)
+
+    await writer.drain()     # Drain the writer buffer
+    await writer.wait_closed()  # Wait until writer is closed
+    #print("Client disconnected")  # Print message when client is disconnected
 
 if __name__=="__main__":
     createWap()
-    createWeb()
-    init()
-    main()
+    try:
+        asyncio.run(main())  # Run the main asynchronous function
+    finally:
+        asyncio.new_event_loop() #Create a new event loop
--- Sketches/Sensor/Sensor.ino
+++ Sketches/Sensor/Sensor.ino
@@ -1,28 +1,83 @@
+// !IMPORTANT  set clockspeed do 1MHz
+
 #include <Wire.h>
+#include <EEPROM.h>
 
 #define PIN A3
 #define LED 1
-#define ADDR 0x08 // individual adress for each chip, starting with 0x08
+#define ADDR 0x14  // individual adress for each chip, starting with 0x08
 
-uint16_t state = 0 ;
+// EEPROM
+#define EEDEVTYPE 0x01
+#define EEDEVMODE 0x02
+
+uint16_t state = 0;
+uint8_t deviceType = 0;  // 0: undefined; 1: LDR Sensor; 2: start buzzer; 3: end buzzer
+uint8_t deviceMode = 0;  // 0: get type; 1: curent value
+
+void setDeviceType(uint8_t t);
+void setDeviceMode(uint8_t m);
+void receiveEvent();
+void requestEvent();
 
 void setup() {
   Wire.begin(ADDR);
   Wire.onRequest(requestEvent);
+  Wire.onReceive(receiveEvent);
   pinMode(LED, OUTPUT);
+  EEPROM.get(EEDEVTYPE, deviceType);
+  EEPROM.get(EEDEVMODE, deviceMode);
 }
 
 void loop() {
-  state = (uint16_t) analogRead(PIN);
+  state = (uint16_t)analogRead(PIN);
   analogWrite(LED, map(state, 0, 1023, 0, 255));
   delay(100);
 }
 
+void receiveEvent() {
+  uint8_t r = Wire.read();
+  uint8_t t = (r >> 4);
+  uint8_t d = (r & 0b00001111);
+  switch (t) {
+    case 0b0001:
+      setDeviceMode(d);
+      break;
+    case 0b0010:
+      setDeviceType(d);
+      break;
+  }
+}
 void requestEvent() {
-  uint16_t value = state;
-  uint8_t arr[2];
-  arr[1]=value & 0xff;
-  arr[0]=(value >> 8);
-  Wire.write(arr[0]);
-  Wire.write(arr[1]);
+  switch (deviceMode) {
+    default:
+    case 0:
+    case 255:
+      Wire.write(deviceType);
+      break;
+    case 1:
+      switch (deviceType) {
+        default:
+        case 0:
+        case 255:
+          break;
+        case 1:
+          uint16_t value = state;
+          uint8_t arr[2];
+          arr[1] = value & 0xff;
+          arr[0] = (value >> 8);
+          Wire.write(arr[0]);
+          Wire.write(arr[1]);
+          break;
+      }
+      break;
+  }
+}
+void setDeviceType(uint8_t t) {
+  deviceType = t;
+  EEPROM.put(EEDEVTYPE, deviceType);
+}
+void setDeviceMode(uint8_t m) {
+  deviceMode = m;
+  EEPROM.put(EEDEVMODE, deviceMode);
 }
